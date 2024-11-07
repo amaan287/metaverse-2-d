98df031f37979136a0e8a0df59259ad2a240f411
amaan287 <amaanmirza287@gmail.com>
2024-11-07 23:33:08 +0530

    re committing

---
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { userRouter } from "./user";
+import { spaceRouter } from "./spaces";
+import { adminRouter } from "./admin";
+export const router = Router();
+
+router.post("/signup", (req, res) => {
+  res.json({
+    message: "signup",
+  });
+});
+router.post("/signin", (req, res) => {
+  res.json({
+    message: "signin",
+  });
+});
+
+router.get("/elements", (req, res) => {});
+router.get("/avatars", (req, res) => {});
+
+router.use("/user", userRouter);
+router.use("/space", spaceRouter);
+router.use("/admin", adminRouter);
