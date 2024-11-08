ee35af4213de2881f2a997998acbceec7103501a
amaan287 <amaanmirza287@gmail.com>
2024-11-08 23:10:53 +0530

    skeleton endpoints

--- metaverse/apps/http/src/routes/v1/admin.ts
+++ metaverse/apps/http/src/routes/v1/admin.ts
@@ -1,3 +1,7 @@
 import { Router } from "express";
 
 export const adminRouter = Router();
+adminRouter.post("/elements", (req, res) => {});
+adminRouter.put("/elements/:elementId", (req, res) => {});
+adminRouter.get("/avatar", (req, res) => {});
+adminRouter.get("/map", (req, res) => {});
