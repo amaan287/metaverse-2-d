2b2c3d9a4da14e5806be4c7cebb430665d381f45
amaan287 <amaanmirza287@gmail.com>
2024-11-09 19:37:46 +0530

    exporting and importing db

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { router } from "./routes/v1";
-
+import client from "@repo/db/client";
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use("/api/v1", router);
